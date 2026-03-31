b39b3d8764e925e589e6ad2d5eb51ac5dce1e2c4
arangrhie <arrhie@gmail.com>
2026-03-31 16:34:16 -0400

    formatting

--- pipeline/bwa/_submit_bwa.sh
+++ pipeline/bwa/_submit_bwa.sh
@@ -32,28 +32,32 @@ if [ ! -s $ref.fai ]; then
 	extra="--dependency=$index"
 fi
 
-cpus=24
-mem=120g
-name=$out.bwa
-script=$VGP_PIPELINE/bwa/bwa.sh
-args="$ref $fastq_map $out"
-partition=norm
-walltime=1-0
-path=`pwd`
-log=logs/$name.%A_%a.log
+if [ ! -s bam.list ]; then
+  cpus=24
+  mem=120g
+  name=$out.bwa
+  script=$VGP_PIPELINE/bwa/bwa.sh
+  args="$ref $fastq_map $out"
+  partition=norm
+  walltime=2-0
+  path=`pwd`
+  log=logs/$name.%A_%a.log
 
-LEN=`wc -l $fastq_map | awk '{print $1}'`
-extra="--array=1-$LEN $extra"
+  LEN=`wc -l $fastq_map | awk '{print $1}'`
+  extra="--array=1-$LEN $extra"
 
-echo "\
-sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args"
-sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args | awk '{print $NF}' > bwa.jid
-wait_for="--dependency=afterok:"`cat bwa.jid`
+  echo "\
+  sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args"
+  sbatch -J $name --mem=$mem --partition=$partition --cpus-per-task=$cpus -D $path $extra --time=$walltime --error=$log --output=$log $script $args | awk '{print $NF}' > bwa.jid
+  wait_for="--dependency=afterok:"`cat bwa.jid`
 
-for i in $(seq 1 $LEN)
-do
-	echo "$out.$i.bam" >> bam.list
-done
+  for i in $(seq 1 $LEN)
+  do
+    echo "$out.$i.bam" >> bam.list
+  done
+else
+  wait_for=""
+fi
 
 cpus=24
 mem=48g
